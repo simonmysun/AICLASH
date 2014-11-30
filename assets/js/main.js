b24e05aab12b6db5a940ecd1e6c41586825cf6c5
simonmysun <simonmysun@gmail.com>
2014-12-01 05:52:50 +0800

    btn event missing

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ var game = new Game();
 var painter;
 
 $(document).ready(function() {
+    $('#code-editor-btn').click(function() {
+        $('#code-editor-wrap').toggleClass('code-editor-visible');
+        $('#code-editor-btn').find('span').toggleClass('glyphicon-chevron-down').toggleClass('glyphicon-chevron-up');
+    });
     var editor = ace.edit("code-editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
