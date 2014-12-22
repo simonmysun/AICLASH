879755044caaef00527a64d1da8ad519e976af80
simonmysun <simonmysun@gmail.com>
2014-12-22 20:24:46 +0800

    use requestAnimationFrame instead of setTimeout

--- assets/js/lib/painter.js
+++ assets/js/lib/painter.js
@@ -1,3 +1,30 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||    // Webkit中此取消方法的名字变了
+                                      window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+            var id = window.setTimeout(function() {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+    }
+}()); // from http://www.zhangxinxu.com/wordpress/2013/09/css3-animation-requestanimationframe-tween-%E5%8A%A8%E7%94%BB%E7%AE%97%E6%B3%95/
+
 function Painter(canvas, game, delay) {
     var self = this;
     var paintQueue = [];
@@ -69,7 +96,7 @@ function Painter(canvas, game, delay) {
             paintCell(c.x, c.y);
             k -- ;
         }
-        setTimeout(render, self.delay);
+        requestAnimFrame(render);
     };
     render();
 }
